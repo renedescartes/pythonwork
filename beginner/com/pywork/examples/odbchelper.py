c746a2e5983a73eaa74630b46e1b64c4d8fbe961
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-07 10:28:02 +0000

    stuff

--- beginner/com/pywork/examples/odbchelper.py
+++ beginner/com/pywork/examples/odbchelper.py
@@ -1,11 +1,16 @@
+__author__ = 'kannan'
+
 def buildConnectionString(params):
-    """Build a connection string from a dictionary of parameters.
-   Returns string."""
+    """Build a connection string from a dictionary
+
+     Returns string.
+     """
     return ";".join(["%s=%s" % (k, v) for k, v in params.items()])
+
 if __name__ == "__main__":
     myParams = {"server":"mpilgrim",
                 "database":"master",
                 "uid":"sa",
                 "pwd":"secret"
     }
-print buildConnectionString(myParams)
+    print buildConnectionString(myParams)
