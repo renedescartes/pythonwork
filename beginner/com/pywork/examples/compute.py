1a059b7dfd49d7edc24c89d9df8765f196cd0101
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-08 17:54:28 +0000

    stuff

---
+++ beginner/com/pywork/examples/compute.py
@@ -0,0 +1,23 @@
+__author__ = 'kannan'
+
+def subMatrix(matrix, rowNumber):
+    if len(matrix) <= 0:
+        raise Exception("Please pass matrix of non zero length")
+    if len(matrix) != len(matrix[0]):
+        raise Exception("Please pass a square matrix")
+    newMatrix = matrix[:]
+    del newMatrix[rowNumber]
+    for row in newMatrix:
+        del row[0]
+    return newMatrix
+
+def determinant(matrix):
+    if len(matrix) <= 0:
+        raise Exception("Please pass matrix of non zero length")
+    if len(matrix) == 1:
+        return matrix[0][0]
+    return sum([determinant(subMatrix(matrix, rowNumber)) * matrix[rowNumber][0] * pow(-1, rowNumber+1) for rowNumber in range(0, len(matrix))])
+
+
+if __name__ == "__main__":
+    print determinant([[3, 1, 2], [2, 0, 3], [4, 1, 2]])
