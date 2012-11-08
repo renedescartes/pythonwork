6d35bacc2839d53dec909949904cfeef891d8929
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-08 18:13:39 +0000

    stuff

--- beginner/test/pywork/examples/computetest.py
+++ beginner/test/pywork/examples/computetest.py
@@ -5,12 +5,14 @@ import unittest
 
 class TestCompute(unittest.TestCase):
 
+    def setUp(self):
+        self.matrix = [[-2, 2, 3], [-1, 1, 3], [2, 0, -1]]
+
     def test_sub_matrix(self):
-        matrix = [[-2, 2, 3], [-1, 1, 3], [2, 0, -1]]
-        self.assertEqual(compute.subMatrix(matrix, 2), [[2, 3], [1, 3]])
+        self.assertEqual(compute.subMatrix(self.matrix, 2), [[2, 3], [1, 3]])
 
     def test_determinant(self):
-        self.assertEqual(compute.determinant([[-2, 2, 3], [-1, 1, 3], [2, 0, -1]]), 6)
+        self.assertEqual(compute.determinant(self.matrix), 6)
 
 if __name__ == '__main__':
     unittest.main()
