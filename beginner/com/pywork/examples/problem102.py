c929fb82b8d0e702ed1b49a5e00f73cf24907da6
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-09 12:39:43 +0000

    stuff

--- beginner/com/pywork/examples/problem102.py
+++ beginner/com/pywork/examples/problem102.py
@@ -1,2 +1,6 @@
 def area(p1, p2, p3):
-    return abs(p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1]))/2;
+    return abs(p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1]))
+
+def contains_origin(p1, p2, p3):
+    origin = (0,0)
+    return area(p1, p2, origin) + area(p1, p3, origin) + area(p2, p3, origin) == area(p1, p2, p3)
--- beginner/test/pywork/examples/problem102test.py
+++ beginner/test/pywork/examples/problem102test.py
@@ -8,10 +8,17 @@ class TestProblem102(unittest.TestCase):
         self.p1 = (-340, 495)
         self.p2 = (-153, -910)
         self.p3 = (835, -947)
+        self.q1 = (-175, 41)
+        self.q2 = (-421, -714)
+        self.q3 = (574, -645)
 
     def test_area(self):
-        self.assertEqual(problem102.area(self.p1, self.p2, self.p3), 690610)
-        self.assertEqual(problem102.area((0, 3), (4, 0), (0, 0)), 6)
+        self.assertEqual(problem102.area(self.p1, self.p2, self.p3), 1381221)
+        self.assertEqual(problem102.area((0, 3), (4, 0), (0, 0)), 12)
+
+    def test_contains(self):
+        self.assertTrue(problem102.contains_origin(self.p1, self.p2, self.p3))
+        self.assertFalse(problem102.contains_origin(self.q1, self.q2, self.q3))
 
 if __name__ == '__main__':
     unittest.main()
