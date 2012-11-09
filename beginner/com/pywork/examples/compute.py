89f0a1a10ee831c4bb9eb24a50ebed226e33b478
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-09 09:27:00 +0000

    stuff

--- beginner/com/pywork/examples/compute.py
+++ beginner/com/pywork/examples/compute.py
@@ -1,5 +1,3 @@
-__author__ = 'kannan'
-
 def subMatrix(matrix, rowNumber):
     newMatrix = [row[1:] for row in matrix]
     del newMatrix[rowNumber]
@@ -8,6 +6,3 @@ def subMatrix(matrix, rowNumber):
 def determinant(matrix):
     return matrix[0][0] if len(matrix) == 1 else \
         sum([determinant(subMatrix(matrix, rowNumber)) * matrix[rowNumber][0] * pow(-1, rowNumber+1) for rowNumber in range(0, len(matrix))])
-
-if __name__ == "__main__":
-    print determinant([[-2, 2, 3], [-1, 1, 3], [2, 0, -1]])
