09ae131e19df7dc2d83299417735c74d62c1194e
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-09 14:49:03 +0000

    stuff

--- beginner/com/pywork/examples/problem102.py
+++ beginner/com/pywork/examples/problem102.py
@@ -1,4 +1,5 @@
 import os
+from com.pywork.examples.runutility import timing
 
 def area(p1, p2, p3):
     return abs(p1[0] * (p2[1] - p3[1]) + p2[0] * (p3[1] - p1[1]) + p3[0] * (p1[1] - p2[1])) / float(2)
@@ -13,8 +14,10 @@ def read_line(line):
     list = line.rstrip().split(",")
     return map(int, list[0:2]), map(int, list[2:4]), map(int, list[4:6])
 
+def full_path(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
+
+@timing
 def answer(filename):
     return sum([1 if contains_origin(pointSet[0], pointSet[1], pointSet[2]) else 0 for pointSet in (read_points(filename))])
 
-def full_path(filename):
-    return os.path.join(os.path.dirname(__file__), filename)
