bf2c38fd952322fd1041adf173bf8cc84acec41b
Random Thoughts <nambi.rangarajan.sujatha@gmail.com>
2012-11-12 14:22:35 +0000

    stuff

--- beginner/com/pywork/examples/problem104.py
+++ beginner/com/pywork/examples/problem104.py
@@ -3,3 +3,20 @@ ALL_DIGIT_SET = set([str(i) for i in range(1, 10)])
 def is_pandigital(n):
     digitSet = set(str(n))
     return len(str(n)) == 9 and len(digitSet) == 9 and len(digitSet - ALL_DIGIT_SET) == 0
+
+def answer():
+    a , b = 1, 1
+    while True:
+
+        return 1
+
+def add_big_number(list1, list2):
+    answer, rlist1, rlist2 = [], list1[::-1], list2[::-1]
+    carry = 0
+    for d in range(0, max(len(list1), len(list2))):
+        total = carry + (rlist1[d] if d < len(rlist1) else 0) + (rlist2[d] if d < len(rlist2) else 0)
+        answer.append(total % 10)
+        carry = total/10
+    if carry > 0:
+        answer.append(carry)
+    return answer[::-1]
--- beginner/test/pywork/examples/problem104test.py
+++ beginner/test/pywork/examples/problem104test.py
@@ -12,5 +12,10 @@ class TestProblem104(unittest.TestCase):
         self.assertFalse(problem104.is_pandigital(4312876))
         self.assertFalse(problem104.is_pandigital(4312598769))
 
+    def test_add_big_number(self):
+        self.assertEquals(problem104.add_big_number([2, 3, 4, 5, 9, 1], [4, 2, 2]), [2, 3, 5, 0, 1, 3])
+        self.assertEquals(problem104.add_big_number([3], [1]), [4])
+        self.assertEquals(problem104.add_big_number([3], [9]), [1, 2])
+
 if __name__ == '__main__':
     unittest.main()
